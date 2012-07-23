0883b7b6902412f8e4fb34bb7c7c4c3f9f7f7d7b
Ian Barber <ian.barber@gmail.com>
2012-07-23 17:50:50 +0100

    Initial checkin of plussytodo
    Initial checkin of sample code - note at the moment the back end isn't at all secured - you could go and read anyones todo list if you knew their user number on plus, as this is just demonstrating the client side flow.

---
+++ todo/js/main/main.js
@@ -0,0 +1,164 @@
+var STORE_KEY = 'plussytodo-auth';
+var auth;
+var user;
+
+// Require the other modules
+require(["jquery", "gplusclient", "plusone"]);
+
+// Load in from local storage
+function gapiloaded() {
+	time = new Date(); 
+
+	// Check if we have localstorage, and make callback if so
+	item = localStorage.getItem(STORE_KEY);
+	if(item) {
+		item = JSON.parse(item);
+		if(time.getTime() - item.time < 3600 * 1000) {
+			onSignInCallback(item.auth)
+		}
+	}	
+}
+
+/* 	This function is triggered when we sign in with the G+ button, and so 
+	is the main entry point into our application. We need to check the 
+	auth result, then retrieve information about the user */
+function onSignInCallback(authResult) {
+	
+	// If authentication didn't succeed, just bail out.
+	if (authResult.error != null) {
+		alert("Sign In Failed");
+		return;
+	}
+	time = new Date(); 
+	
+	// Store auth object to local storage. 
+	localStorage.setItem(STORE_KEY, JSON.stringify({auth: authResult, time: time.getTime()}) );
+	
+	auth = authResult;
+	gapi.auth.setToken(auth);
+	
+	// Retrieve the user's profile information
+	var args = {
+		'path': '/plus/v1/people/me?fields=displayName,id,image',
+		'method': 'GET',
+		'callback': function (response) {
+			// We can now display our todo list, and personalise the page
+			$("H1").html(response.displayName + "'s Todos");
+			$("H1").css("background-image", 'url(' + response.image.url + ')');
+			$("H1").addClass('signedin');
+			
+			// We hide the sign in button
+			$("#signin").toggle();
+			
+			// In case there was any question about this being a demo
+			user = response.id;
+			
+			// Retrieve a list of TODOs
+			getItems(user);
+		}
+	};
+	gapi.client.request(args);
+}
+
+/* Retrieve the items for the user from our appengine app */
+function getItems(email) {
+	$.ajax({
+		url:  "/list",
+		cache: false,
+		data: { user: email }
+	}).done(function( html ) {
+		// We retrieve HTML from the app, so we can add that
+		$("#listcontainer").append(html);
+		
+		// Set up the event handler for the 'done' click
+		$("body").delegate(".rem", "click", checkOff);
+		
+		// Setup the event handler for the new click
+		$(".add-to-do").submit(function(e) {
+			e.preventDefault();
+			type = $(this).attr("data-itemname");
+
+			// Add the todo to the remote storage
+			entry = $("#" + type + "-entry").val();
+			storeItem(user, type, entry);
+
+			// Blank the box
+			$("#" + type + "-entry").val("");
+		});
+	});
+}
+
+/* 	This function stores the todo to the backend, 
+	and pushes a new moment to the Google+ history
+	API */
+function storeItem(user, type, entry) {
+	// Call out to the backend
+	$.post(
+	  	"/list",
+		{ user: user, type: type, entry: entry },
+		function( link, status, jqXHR) {
+	  		// Add the LI
+			$("#" + type + "-todo-items").append(link);
+			
+			// We pass the real location as a custom X-Header, as
+			// there's a redirect involved
+			url = jqXHR.getResponseHeader("X-Item-Location");
+
+			// Make the Create moment
+			moment = {
+				"type":"http://schemas.google.com/CreateActivity",
+				"target":{
+					"url": url
+				}
+			};
+			
+			// Finally, we create the moment and push it to +
+			pushMoment(moment);
+		});
+}
+
+/* This function marks a task as 'done' and pushes a moment to + */
+function checkOff(e) {
+	e.preventDefault();
+	// We store the item details as data- attributes
+	var itemid = $(this).attr("data-itemid");
+	var itemtype = $(this).attr("data-itemtype");
+	
+	$.post(
+	  "/list/" + itemid + "/type/" + itemtype,
+		{ user: user },
+		function( done ) {
+			// Remove the LI
+			$("#" + itemid).fadeOut(function() {
+			        $("#" + itemid).remove();
+			});
+			
+			// Get the URL
+			url = jqXHR.getResponseHeader("X-Item-Location");
+			
+			// We use an AddActivity as there's no better matching type
+			moment = {
+				"type":"http://schemas.google.com/AddActivity",
+				"target":{
+					"url": url
+				}
+			};
+			
+			pushMoment(moment);
+		});
+}
+
+/* This function adds the moment to the History API */
+function pushMoment(moment) {
+    gapi.auth.setToken(auth);
+
+    var args = {
+      'path': '/plus/v1moments/people/me/moments/vault?debug=true',
+      'method': 'POST',
+      'body': JSON.stringify(moment),
+      'callback': function(response) {
+		// We probably would want to check for errors here!
+      }
+    };
+    gapi.client.request(args);
+}
---
+++ todo/todo.go
@@ -0,0 +1,258 @@
+package todo
+
+// TODO: Validate user on removing
+// TODO: Be clever about staying authorised 
+// TODO: Put oauth token in local storage
+// TODO: Refresh oauth token if expired
+
+import (
+	"appengine"
+	"appengine/datastore"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type TodoItem struct {
+	Id         string
+	User       string
+	Entry      string
+	Date       time.Time
+	IsComplete bool
+	Type       string
+	Key        *datastore.Key `datastore:"-"`
+}
+
+type TodoList struct {
+	Name    string
+	Title   string
+	Imurl   string
+	Done    string
+	Pending string
+	Items   []TodoItem
+}
+
+/* Main entry function on app engine */
+func init() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/list", managelist)
+	http.HandleFunc("/list/", manageitem)
+}
+
+/**
+ ** Web Handler Functions
+ **/
+
+/* Render the main screen */
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, nil)
+}
+
+/* View or add to list */
+func managelist(w http.ResponseWriter, r *http.Request) {
+	context := appengine.NewContext(r)
+	
+	if r.Method == "POST" {
+		/* If we're POSTing, create a new item */
+		todo := TodoItem{
+			User:       r.FormValue("user"),
+			Entry:      r.FormValue("entry"),
+			Date:       time.Now(),
+			Type:       r.FormValue("type"),
+			IsComplete: false,
+		}
+
+		url, err := todo.store(context)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, url, http.StatusFound)
+	} else {
+		/* If GETing, retrieve the list of user todos */
+		var lists []TodoList = getdefaultlists()
+		for i := range lists {
+			lists[i].getitems(context, r.FormValue("user"))
+		}
+		
+		itemsTemplate.Execute(w, map[string]interface{}{"Lists": lists})
+	}
+}
+
+/* View or update specific todos */
+func manageitem(w http.ResponseWriter, r *http.Request) {
+	context := appengine.NewContext(r)
+	
+	/* Retrieve the item we're working with */
+	itemp, err := getitem(context, r.URL.Path)
+	item := *itemp
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	/* If we're not getting it from ajax, switch to pretty template */
+	if r.Header.Get("X-Requested-With") != "XMLHttpRequest" {
+		itemlist := item.getlist()
+
+		err = plusTemplate.Execute(w, map[string]interface{}{
+			"IsComplete": item.IsComplete,
+			"Type":       itemlist.Name,
+			"Baseurl":    baseurl(),
+			"Imurl":      itemlist.Imurl,
+			"Entry":      item.Entry,
+			"Done":       itemlist.Done,
+			"Pending":    itemlist.Pending,
+		})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	/* If we're POSTing to this, we're checking it off */
+	if r.Method == "POST" {
+		err = item.checkoff(context)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	/* Otherwise, just render the item */
+	w.Header().Add("X-Item-Location", fmt.Sprintf("%s%s", baseurl(), r.URL.Path))
+	err = itemTemplate.Execute(w, item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+
+/**
+ ** TodoItem Methods
+ **/
+
+/* Update a todo item and mark as complete */
+func (t *TodoItem) checkoff(context appengine.Context) (error) {
+	todo := *t
+	todo.IsComplete = true
+	key := datastore.NewKey(context, "Todos-" + todo.Type, todo.Key.StringID(), todo.Key.IntID(), nil)
+	_, err := datastore.Put(context, key, &todo)
+	*t = todo
+	return err
+}
+
+/* Retrieve the matching item list for a todo item */
+func (t *TodoItem) getlist() (TodoList) {
+	todo := *t
+	var lists []TodoList = getdefaultlists()
+	itemlist := lists[0]
+	for i := range lists {
+		if lists[i].Name == todo.Type {
+			itemlist = lists[i]
+			break
+		}
+	}
+	return itemlist
+}
+
+/* Save a todo item */
+func (t *TodoItem) store(context appengine.Context) (string, error) {
+	todo := *t
+	key, err := datastore.Put(context, datastore.NewIncompleteKey(context, "Todos-"+ todo.Type, nil), &todo)
+	if err != nil {
+		return "", err
+	}
+	url := fmt.Sprintf("%s/list/%s/type/%s", baseurl(), key.Encode(), todo.Type)
+	return url, nil
+}
+
+/**
+ ** Todolist Methods
+ **/
+
+/* Retrieve a list of items for the todo list for a given user */
+func (t *TodoList) getitems(context appengine.Context, user string) {
+	tl := *t
+	q := datastore.NewQuery("Todos-"+tl.Name).Filter("User =", user)
+	q = q.Filter("IsComplete = ", false).Order("Date").Limit(1000)
+	key, _ := q.GetAll(context, &tl.Items)
+	for i := range tl.Items {
+		tl.Items[i].Key = key[i]
+	}
+	*t = tl
+}
+
+/**
+ ** Support Functions
+ **/
+
+/* Retrieve the appropriate base URL for the environment */
+func baseurl() string {
+	if appengine.IsDevAppServer() {
+		return "http://localhost:8080"
+	}
+	return "http://plussytodo.appspot.com"
+}
+
+/* Given a URL path, retrieve the corresponding item */
+func getitem(context appengine.Context, url string) (*TodoItem, error) {
+	parts := strings.Split(strings.Replace(url, "/list/", "", 1), "/type/")
+	id := parts[0]
+
+	key, err := datastore.DecodeKey(id)
+	if err != nil {
+		return nil, err
+	}
+
+	item := new(TodoItem)
+	err = datastore.Get(context, key, item)
+	if err != nil {
+		return nil, err
+	}
+	
+	item.Key = key
+
+	return item, nil
+}
+
+/* Get the default array lists */
+func getdefaultlists() []TodoList {
+	var lists []TodoList = make([]TodoList, 4)
+	lists[0].Name = "work"
+	lists[0].Title = "Work"
+	lists[0].Imurl = "images/noun_project_1539"
+	lists[0].Done = "A Completed Task"
+	lists[0].Pending = "A Task To Do"
+	lists[1].Name = "home"
+	lists[1].Title = "Home"
+	lists[1].Done = "A Completed Tas"
+	lists[1].Pending = "A Task To Do"
+	lists[1].Imurl = "images/noun_project_293"
+	lists[2].Name = "calls"
+	lists[2].Title = "Calls"
+	lists[2].Done = "A Made Call"
+	lists[2].Pending = "A Call To Make"
+	lists[2].Imurl = "images/noun_project_127"
+	lists[3].Name = "errands"
+	lists[3].Title = "Errands"
+	lists[3].Done = "A Completed Errand"
+	lists[3].Pending = "An Errand To Run"
+	lists[3].Imurl = "images/noun_project_3364"
+	return lists
+}
+
+
+/**
+ ** Templates
+ **/
+
+var homeTemplate = template.Must(template.ParseFiles("todo/templates/main.html"))
+var itemTemplate = template.Must(template.ParseFiles("todo/templates/entry.html"))
+var plusTemplate = template.Must(template.ParseFiles("todo/templates/moment_plus.html"))
+var itemsTemplate = template.Must(template.ParseFiles("todo/templates/list.html"))
+var entryTemplate = template.Must(itemsTemplate.ParseFiles("todo/templates/entry.html"))
